a78d943688830052cc1f19ffd1470d4c5ae46ea5
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-12-29 14:11:03 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CurrencyConverter from './components/currencychanger'
+
+function App() {
+  return (
+
+
+    <div className="flex flex-wrap" style={{textAlign:"center",justifyContent:"center"}}>
+
+<CurrencyConverter/>
+    </div>
+
+  )
+}
+export default App
